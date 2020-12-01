a4b672f0692f49aceb138c9257b1649f3f2c5175
hunyiha <hunyiha@gmail.com>
2020-12-01 09:04:25 +0800

    完善日志

--- src/main/java/com/example/fileUploadDownload/controller/FileController.java
+++ src/main/java/com/example/fileUploadDownload/controller/FileController.java
@@ -1,12 +1,16 @@
 package com.example.fileUploadDownload.controller;
 
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.ZipUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import com.example.fileUploadDownload.util.TestFileUtil;
 import com.example.fileUploadDownload.vo.PoliceInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class FileController {
 
     @Value("${file.police.original}")
@@ -49,14 +54,25 @@ public class FileController {
         // 解压
         ZipUtil.unzip(file.getInputStream(), new File(filePoliceOriginal), Charset.defaultCharset());
 
+        log.info("解压: {}解压完成",file.getOriginalFilename());
+
         File originalFile = new File(filePoliceOriginal);
 
         File[] files = originalFile.listFiles();
 
+        if(ArrayUtil.isEmpty(files)){
+            return "上传压缩包中没有文件";
+        }
+
         List<String> fileNameList = new ArrayList<>();
 
         for (File imageFile : files) {
-            fileNameList.add(imageFile.getName());
+            if(imageFile.getName().endsWith(".png")){
+                fileNameList.add(imageFile.getName());
+            }else{
+                log.info("异常文件: {}文件格式不是jpg",imageFile.getName());
+            }
+
         }
 
         List<PoliceInfo> policeInfos = new ArrayList<>();
@@ -70,16 +86,21 @@ public class FileController {
                 policeInfo.setAge(nodes[1]);
                 policeInfo.setLocation(nodes[2]);
                 policeInfos.add(policeInfo);
+            }else{
+                log.info("文件名称格式: {}文件命名格式不正确",fileName);
             }
         }
 
+        // 数据填充
         listFill(policeInfos);
 
+        // 数据压缩打包
         ZipUtil.zip("D:\\001file\\police",filePoliceZip+File.separator+ "police.zip", false);
 
         File downLoadFile = new File(filePoliceZip+File.separator+ "police.zip");
         if (!downLoadFile.exists()) {
-            return "-1";
+            log.info("压缩文件: {}不存在", filePoliceZip+File.separator+ "police.zip");
+            throw new RuntimeException("压缩文件不存在");
         }
 
         response.reset();
@@ -113,6 +134,12 @@ public class FileController {
     }
 
     public void listFill(List<PoliceInfo> policeInfoList) {
+
+        if(CollUtil.isEmpty(policeInfoList)){
+            log.info("excel填充: 不存在填充数据为空");
+            throw new RuntimeException("不存在填充数据");
+        }
+
         // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
         // 填充list 的时候还要注意 模板中{.} 多了个点 表示list
         String templateFileName = "D:\\000_config" + File.separator + "police.xlsx";
